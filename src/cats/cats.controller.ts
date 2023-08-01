8b237dcf74dbfff788e7d339611d7faf7945baf9
junkiYoon <juneki4933@gmail.com>
2023-08-01 17:08:22 +0900

    overview/providers example code

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,39 +1,19 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
-import { UpdateCatDto } from './dto/update-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
-    return `This action adds a new cat (${createCatDto.name}, ${createCatDto.age}, ${createCatDto.breed})`;
+    this.catsService.create(createCatDto);
   }
 
   @Get()
-  async findAll(): Promise<any[]> {
-    return [];
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return `This action updates a #${id} cat (${updateCatDto.name}, ${updateCatDto.age}, ${updateCatDto.breed})`;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 }
