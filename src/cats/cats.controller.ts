6317976a1b47832b45d6d60cb1a5986d81d9719d
junkiYoon <juneki4933@gmail.com>
2023-08-01 16:29:11 +0900

    overview/controllers example code

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return `This action adds a new cat (${createCatDto.name}, ${createCatDto.age}, ${createCatDto.breed})`;
+  }
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat (${updateCatDto.name}, ${updateCatDto.age}, ${updateCatDto.breed})`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
