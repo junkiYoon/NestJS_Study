c97273946a2cef755aa19e50ec1f0e1311c35254
junkiYoon <juneki4933@gmail.com>
2023-08-02 16:08:25 +0900

    overview/guards example code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsController } from './cats/cats.controller';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from './common/pipes/validation.pipe';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_PIPE, useClass: ValidationPipe }],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useClass: ValidationPipe },
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
